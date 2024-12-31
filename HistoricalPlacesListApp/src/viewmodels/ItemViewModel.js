187d5bf50597ff119ce2703a8c07a171e5c50790
Hasibul Hassan <shahi_aust@hotmail.com>
2024-12-31 22:10:17 +0600

    add json data source

--- HistoricalPlacesListApp/src/viewmodels/ItemViewModel.js
+++ HistoricalPlacesListApp/src/viewmodels/ItemViewModel.js
@@ -1,19 +1,16 @@
 import { useState, useEffect } from 'react';
 
+// Import the JSON file
+import placesData from '../places.json'; //'./place.json';
+
 export const useItemViewModel = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Simulate fetching data
+    // Simulating fetching local JSON data
     setTimeout(() => {
-      const fetchedItems = [
-        { id: 1, title: 'Item 1', description: 'Description for item 1', visited: false },
-        { id: 2, title: 'Item 2', description: 'Description for item 2', visited: true },
-        { id: 3, title: 'Item 3', description: 'Description for item 3', visited: false },
-        { id: 4, title: 'Item 4', description: 'Description for item 4', visited: true },
-      ];
-      setItems(fetchedItems);
+      setItems(placesData);
       setIsLoading(false);
     }, 1000);
   }, []);
@@ -31,4 +28,3 @@ export const useItemViewModel = () => {
     toggleVisited,
   };
 };
-
